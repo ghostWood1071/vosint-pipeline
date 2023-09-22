fb464c35696e9a23456d37962a177681cf596245
ghostWood1071 <thinhquangshin@gmail.com>
2023-09-22 14:42:46 +0700

    update crawl ttxvn

--- features/job/services/crawling_ttxvn.py
+++ features/job/services/crawling_ttxvn.py
@@ -6,24 +6,25 @@ def select(from_element, expr, by = "css="):
         element = [element.nth(i) for i in range(element.count())]
         return element
 def crawl_ttxvn(username,password,id_news):
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch()
-        page = browser.new_page()
-        try:
-            page.goto('https://news.vnanet.vn/')
-            page.click('#btnSignIn')
+    playwright = sync_playwright().start()
+    browser = playwright.chromium.launch(channel="chrome")
+    page = browser.new_page()
+    try:
+        page.goto('https://news.vnanet.vn/')
+        page.click('#btnSignIn')
 
-            page.fill('#username', username)
-            page.fill('#password', password)
+        page.fill('#username', username)
+        page.fill('#password', password)
 
-            page.click('#login')
-            time.sleep(1)
-            
+        page.click('#login')
+        time.sleep(1)
+        
 
-            page.goto('https://news.vnanet.vn/FrontEnd/PostDetail.aspx?id='+ str(id_news))
-            content = select(page,".post-content")[0].inner_text()
-        except:
-            page.close()
+        page.goto('https://news.vnanet.vn/FrontEnd/PostDetail.aspx?id='+ str(id_news))
+        content = select(page,".post-content")[0].inner_text()
+    except:
+        page.close()
+        playwright.stop()
     
     return content
     
