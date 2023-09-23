fa1f92d706241f371b605581baf0e11837ff86c0
ghostWood1071 <thinhquangshin@gmail.com>
2023-09-23 11:37:28 +0700

    update facebook crawl

--- automation/actions/facebook_crawler/fb_page.py
+++ automation/actions/facebook_crawler/fb_page.py
@@ -9,52 +9,9 @@ from bson.objectid import ObjectId
 from playwright.sync_api import sync_playwright
 from playwright.sync_api import Browser, Page, Locator
 from typing import *
-from datetime import datetime, timedelta
-from dateutil import parser
-from models import MongoRepository
+from .util import *
 
-def select(from_element: Union[Locator, Page], expr:str):
-        element = from_element.locator(f"{expr}")
-        element = [element.nth(i) for i in range(element.count())]
-        return element
 
-def convert_to_std_date(date_str:str):
-    result = datetime.now()
-    try:
-        if 'hrs' in date_str:
-            hour = int(re.findall(r"\d+", date_str)[0])
-            result = result - timedelta(hours=hour)
-        elif 'mins' in date_str:
-            mins = int(re.findall(r"\d+", date_str)[0])
-            result = result - timedelta(minutes=mins)
-        else:
-            result = parser.parse(date_str, fuzzy=True)
-        return result
-    except Exception as e:
-        print(e)
-        return result
-
-def process_like(likes_string:str):       
-    suffixes = {
-        'K': 1000,
-        'M': 1000000,
-        'B': 1000000000,
-        'T': 1000000000000,
-    }
-    matches = re.search(r'(\d+(\.\d+)?)', likes_string)
-    if matches:
-        number_str = matches.group(1)
-        likes_quantity = float(number_str)
-        remaining_text = likes_string.replace(number_str, '').strip()
-        if remaining_text:
-            for suffix in remaining_text.split(" "):
-                multiplier = suffixes.get(suffix)
-                if multiplier != None:
-                    likes_quantity *= multiplier
-                    break
-        return likes_quantity
-    else:
-        return 0
         
 def get_article_data(article_raw:Locator, crawl_social_id):
     try:
@@ -91,31 +48,11 @@ def get_article_data(article_raw:Locator, crawl_social_id):
         data["post_id"] = json.loads(data_ft).get("mf_story_key")
         data["footer_type"] = "page"
         data["id_social"] = crawl_social_id
+        data["sentiment"] = get_sentiment(data["header"], data["content"])
+        data["keywords"] = get_keywords(data["content"])
         return data
     except:
         raise Exception("post none")
-    
-def scroll_loop(action: Any ,**kwargs:Dict[str, Any]):
-    kwargs.update({'got_article': 0})
-    page:Page = kwargs.get("page")
-    got_quantity = 1
-    while got_quantity>0:
-        page.keyboard.press("End")
-        page.wait_for_selector("body")
-        time.sleep(10)
-        real_got = action(**kwargs)
-        if(real_got==0):
-            break
-        else:
-            got_quantity = real_got
-
-def check_and_insert_to_db(data):
-    is_exists  = MongoRepository().get_one("facebook", {"post_id": data.get("post_id")})
-    if is_exists == None:
-        MongoRepository().insert_one("facebook", data)
-        return True
-    return False
-
 #this is action
 def get_articles(page:Page, got_article:int, crawl_social_id)->bool:
     articles = select(page, "article")
@@ -132,7 +69,6 @@ def get_articles(page:Page, got_article:int, crawl_social_id)->bool:
     return len(articles)
 
 def fb_page(browser:Browser, cookies,link_person, account, password, source_acc_id, crawl_acc_id):
-    data = {}
     page:Page = authenticate(browser, cookies, link_person, account, password, source_acc_id) 
     # articles = select(page, "article")
     # for article in articles:
