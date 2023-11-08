5e8b67c6d9cafddb797de1a563c4c153e3761fe6
ghostWood1071 <thinhquangshin@gmail.com>
2023-11-08 14:13:16 +0700

    update

--- automation/actions/facebook_crawler/authenticate.py
+++ automation/actions/facebook_crawler/authenticate.py
@@ -17,11 +17,11 @@ def login(page, account, password):
     return page.context.cookies()
 
 def authenticate(browser:Browser, cookies:Any, link, account, password, source_acc_id):
-    user_agent = (
-        "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) "
-        "AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1"
-    )
-    context = browser.new_context(user_agent=user_agent)
+    # user_agent = (
+    #     "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) "
+    #     "AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1"
+    # )
+    context = browser.new_context()
     context.add_cookies(cookies)
     page:Page = context.new_page()
     page.set_viewport_size({"width": 375, "height": 812})
