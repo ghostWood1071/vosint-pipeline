822ad74e5e2a27638ac4b82f078d7bc169c400e3
ThinhDQ <thinhquangshin@gmail.com>
2024-04-12 09:11:41 +0700

    update

--- models/kafka_consumer.py
+++ models/kafka_consumer.py
@@ -131,6 +131,12 @@ class KafkaConsumer_class:
                 message_data = message.value
                 try:
                     task = self.get_task(message_data.get("task_id"))
+                    if task is None:
+                        task = {
+                            "executed": False,
+                            "source": "old news",
+                            "url": self.get_url(message_data)
+                        }
                     if task.get("executed"):
                         continue
                     url = task.get("url")
